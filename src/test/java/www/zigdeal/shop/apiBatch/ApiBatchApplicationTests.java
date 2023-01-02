8d5a1cdd6766fb850724d12865d8a8b4ab78e947
Jungwon423 <jungwon423@naver.com>
2023-01-03 03:25:42 +0900

    Initial commit

---
+++ src/test/java/www/zigdeal/shop/apiBatch/ApiBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package www.zigdeal.shop.apiBatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
