e6bbd78ea9d3d5e8d55744e197014dea00f1c576
oreo <ohsimon77@naver.com>
2023-01-03 21:35:19 +0900

    feat : 환율 API로 받아오는 메소드 추가

--- src/main/java/www/zigdeal/shop/apiBatch/batch/readers/ExchangeRateReader.java
+++ src/main/java/www/zigdeal/shop/apiBatch/batch/readers/ExchangeRateReader.java
@@ -7,8 +7,15 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.springframework.batch.item.ItemReader;
 import www.zigdeal.shop.apiBatch.domain.ExchangeRate;
+import org.json.simple.*;
+import org.json.simple.parser.*;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 @RequiredArgsConstructor
 public class ExchangeRateReader implements ItemReader<ExchangeRate> {
@@ -17,6 +24,7 @@ public class ExchangeRateReader implements ItemReader<ExchangeRate> {
     public ExchangeRate read() {
         ExchangeRate exchangeRate = new ExchangeRate();
         crawling(exchangeRate);
+//        APIReader(exchangeRate);
         return exchangeRate;
     }
 
@@ -33,8 +41,7 @@ public class ExchangeRateReader implements ItemReader<ExchangeRate> {
             Double todayExchangeRate = elementToDouble(ele);
             exchangeRate.setName("오늘의 환율");
             exchangeRate.setExchangeRate(todayExchangeRate);
-
-            System.out.println(exchangeRate.toString());
+//            System.out.println(exchangeRate.toString());
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -45,5 +52,50 @@ public class ExchangeRateReader implements ItemReader<ExchangeRate> {
         String text = ele.text();
         String regexText = text.replaceAll(",", "");
         return Double.parseDouble(regexText);
+    }
+
+    public Double ObjectToDouble(Object object){
+        String StringRate = (String) object;
+        String regexText = StringRate.replaceAll(",", "");
+        return Double.parseDouble(regexText);
+    }
+
+    public void APIReader(ExchangeRate exchangeRate) {
+        JSONParser parser = new JSONParser();
+        String AuthKey = "e4lofYKs4QdnWUW6eAoSHTAzJkNncSGf";
+        String SearchDate = "20230103";
+        String dataType = "AP01";
+        String apiURL = "https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=" + AuthKey + "&searchdate=" + SearchDate + "&data=" + dataType;
+        //한국수출입은행 환율
+        try {
+            URL oracle = new URL(apiURL);
+            URLConnection yc = oracle.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                JSONArray a = (JSONArray) parser.parse(inputLine);
+
+                for (Object o : a) {
+                    JSONObject JSONRate = (JSONObject) o;
+                    //System.out.println((String) JSONRate.get("cur_nm"));
+                    if (("미국 달러").equals(JSONRate.get("cur_nm"))){
+                        Double todayExchangeRate = ObjectToDouble(JSONRate.get("deal_bas_r"));
+                        exchangeRate.setName("오늘의 환율");
+                        exchangeRate.setExchangeRate(todayExchangeRate);
+//                    환율 api 데이터 예시에 대한 것은 핫딜 노션 환율 API 관련 참조
+//                    System.out.println("bkpr>>" + (String) JSONRate.get("bkpr"));
+                        System.out.println("deal_bas_r>>" + JSONRate.get("deal_bas_r"));  //매매 기준율 이거 쓰면 될듯 함
+                    }
+                }
+            }
+            in.close();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
