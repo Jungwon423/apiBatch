cfbff1fb2a8d1de2eae4d13e4b7fa632be382f29
oreo <ohsimon77@naver.com>
2023-01-03 23:16:10 +0900

    feat : 간단한 scheduler 코드 추가

---
+++ src/main/java/www/zigdeal/shop/apiBatch/batch/Scheduler.java
@@ -0,0 +1,43 @@
+package www.zigdeal.shop.apiBatch.batch;
+
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecutionException;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+public class Scheduler {
+    @Component
+    @RequiredArgsConstructor
+    public class StoreStationScheduler {
+
+        private final Logger logger = LoggerFactory.getLogger("file");
+
+        private final Job job;
+        private final JobLauncher jobLauncher;
+
+        // 24시간 마다 실행
+        @Scheduled(fixedDelay = 24 * 60 *60 * 1000L)
+        public void executeJob () {
+            try {
+                logger.info("fetch gas station job start");
+                jobLauncher.run(
+                        job,
+                        new JobParametersBuilder()
+                                .addString("datetime", LocalDateTime.now().toString())
+                                .toJobParameters()  // job parameter 설정
+                );
+                logger.info("successfully complete job\n\n");
+            } catch (JobExecutionException ex) {
+                System.out.println(ex.getMessage());
+                ex.printStackTrace();
+            }
+        }
+    }
+}
