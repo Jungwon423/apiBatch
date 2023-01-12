a8bfca55b9839cb23370e525f838d1a7cdd04928
Jungwon423 <44633262+Jungwon423@users.noreply.github.com>
2023-01-12 15:54:49 +0900

    AliExpressReader 추가

--- src/main/java/www/zigdeal/shop/apiBatch/batch/product/ProductConfiguration.java
+++ src/main/java/www/zigdeal/shop/apiBatch/batch/product/ProductConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.data.MongoItemReader;
 import org.springframework.batch.item.data.MongoItemWriter;
 import org.springframework.batch.item.data.builder.MongoItemWriterBuilder;
@@ -13,9 +14,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import www.zigdeal.shop.apiBatch.batch.exchangeRate.domain.ExchangeRate;
+import www.zigdeal.shop.apiBatch.batch.exchangeRate.readers.ExchangeRateReader;
 import www.zigdeal.shop.apiBatch.batch.product.domain.Product;
+import www.zigdeal.shop.apiBatch.batch.product.readers.AliExpressReader;
 
-import javax.batch.api.chunk.ItemReader;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -39,23 +42,28 @@ public class ProductConfiguration {
     public Step ProductStep() {
         return stepBuilderFactory.get("CollectProductStep")
                 .<Product, Product>chunk(10)
-                .reader(productMongoItemReader())
+                .reader(AliExpressItemReader())
                 .writer(productMongoItemWriter())
                 .build();
     }
 
     @Bean
-    public MongoItemReader<Product> productMongoItemReader() {
-        MongoItemReader<Product> mongoItemReader = new MongoItemReader<>();
-        mongoItemReader.setTemplate(mongoTemplate);
-        mongoItemReader.setCollection("products");
-        mongoItemReader.setTargetType(Product.class);
-        mongoItemReader.setQuery("{}");
-        Map<String, Sort.Direction> sort = new HashMap<String, Sort.Direction>(1);
-        sort.put("_id", Sort.Direction.ASC);
-        mongoItemReader.setSort(sort);
-        return mongoItemReader;
+    public ItemReader<Product> AliExpressItemReader() {
+        return new AliExpressReader();
     }
+
+//    @Bean
+//    public MongoItemReader<Product> productMongoItemReader() {
+//        MongoItemReader<Product> mongoItemReader = new MongoItemReader<>();
+//        mongoItemReader.setTemplate(mongoTemplate);
+//        mongoItemReader.setCollection("products");
+//        mongoItemReader.setTargetType(Product.class);
+//        mongoItemReader.setQuery("{}");
+//        Map<String, Sort.Direction> sort = new HashMap<String, Sort.Direction>(1);
+//        sort.put("_id", Sort.Direction.ASC);
+//        mongoItemReader.setSort(sort);
+//        return mongoItemReader;
+//    }
 
     @Bean
     public MongoItemWriter<Product> productMongoItemWriter() {
